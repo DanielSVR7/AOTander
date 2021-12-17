dbd89ca501efe98663c44dad3e1bb68d88d311c2
Даниил Свириденко <sviridenko-02@mail.ru>
2021-12-17 23:31:39 +0400

    Добавьте файлы проекта.

---
+++ AOTander/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using AOTander.Models;
+using AOTander.ViewModels.Base;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace AOTander.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        public DatabaseEntities db = new DatabaseEntities();
+        public ObservableCollection<Shops> Shops { get; }
+        public MainWindowViewModel()
+        {
+            var l = new List<Shops>();
+            foreach (var item in db.Shops)
+            {
+                l.Add(new Shops
+                {
+                    Id = item.Id,
+                    Address = item.Address,
+                    DirectorID = item.DirectorID
+                });
+            }
+            Shops = new ObservableCollection<Shops>(l);
+        }
+    }
+}
